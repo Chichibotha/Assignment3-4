25c091c6f929a8f27f89a1a207bf0c27672cec26
Chimwemwe Botha <152778368+Chichibotha@users.noreply.github.com>
2026-04-17 01:32:55 +0200

    Add files via upload

---
+++ assignment3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    int arr[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+    printf("Method 1: arr[i][j]\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nMethod 2: *(arr[i] + j)\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", *(arr[i] + j));
+        }
+        printf("\n");
+    }
+    printf("\nMethod 3: *(*(arr + i) + j)\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", *(*(arr + i) + j));
+        }
+        printf("\n");
+    }
+    return 0;
+}
